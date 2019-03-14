8c82071db2be5ef9f1610c3e5df8bcad1d0c2dc2
Jesús Ángel Pérez-Roca Fernández <yisus82@users.noreply.github.com>
2019-03-14 10:40:54 +0100

    Balloon Popper

---
+++ Balloon Popper/Assets/Scripts/BalloonController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class BalloonController : MonoBehaviour
+{
+  public float scaleFactor = 1.2f;
+  public float maxScale = 3f;
+
+  private void OnMouseDown()
+  {
+    transform.localScale *= scaleFactor;
+    if (transform.localScale.x >= maxScale
+    || transform.localScale.y >= maxScale
+    || transform.localScale.z >= maxScale)
+    {
+      Destroy(gameObject);
+    }
+  }
+
+  private void OnValidate()
+  {
+    scaleFactor = Mathf.Max(scaleFactor, 1.01f);
+  }
+}
